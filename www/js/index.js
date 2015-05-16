71e7f402d9699fb453592717b3767ca2f78b8dfb
ericBaez <eabhaa@live.com.xm>
2015-05-15 19:53:44 -0500

    ADD

--- www/js/index.js
+++ www/js/index.js
@@ -268,22 +268,70 @@ module.controller('TicketIndividualController', function($scope,$dataTickets) {
       
       return myDataProfile;
   });
-  module.controller('ECommunicator', function($scope, $http) {
+
+module.controller('ECommunicator', function($scope, $http) {
     $http.get("http://empowerlabs.com/intellibanks/data/EmpowerLabsIntra/DBTXTjson.php")
     .success(function (response) {
     	$scope.names = response.arr;
     	});
-$scope.showCom=function(name){
+   $scope.showCom=function(name){
     	selectedCom=name;
 	$scope.ons.navigator.pushPage('detailComunicado.html');
-};
-$scope.nuevoComunicado=function()
+    };
+  $scope.nuevoComunicado=function()
   {$scope.ons.navigator.pushPage('nuevoComunicado.html');};
 });
+
+
+
    	
 module.controller('DetailComunicadoController', function($scope) {
 	$scope.com=selectedCom;
+  
+  $scope.cuentaOne=function(com){
+    selectedCom=com;
+    $scope.ons.navigator.pushPage('editCommunic.html');
+   };
+
 }); 
+
+module.controller('editControlerComunicado', function($scope, $http){
+  //$scope.com=selectedCom;
+  $scope.formD=selectedCom;
+  $scope.formD.autor=mydata.user;
+  //selectedCom=selectedCom;
+  // aqui se van a meter  datos 
+  //$scope.formD={};
+  $scope.loa=function(){
+     modal.show();
+        $http.get('http://empowerlabs.com/intellibanks/data/EmpowerLabsIntra/editComm.php?ht='+$scope.formD.ht+'&asunto='+$scope.formD.asunto+'&descripcion='+$scope.formD.descripcion+'&autor='+$scope.formD.autor+'&categoria='+$scope.formD.categoria+'&area='+$scope.formD.area+'&prioridad='+$scope.formD.prioridad+'&activo='+$scope.formD.activo+'&fecha='+$scope.formD.fecha).
+        success(function(data, status, headers, config) {
+         modal.hide();
+         $scope.alta.$setPristine();
+         $scope.formD = {};
+        ons.notification.alert({message: ''+data.mensaje, title:"Intellibanks"});
+      }).
+      error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+      });
+
+  };
+
+  $scope.cat={};
+   $http.get('http://empowerlabs.com/intellibanks/data/EmpowerLabsIntra/areasecomm.php').
+   success(function(data){
+    $scope.cat=data;
+   });
+    //paso a areas
+    $scope.are={};
+   $http.get('http://empowerlabs.com/intellibanks/data/EmpowerLabsIntra/EComareas.php').
+   success(function(data){
+    $scope.are=data;
+   });
+    
+
+}); //end controler
 
 
 
@@ -305,7 +353,7 @@ ons.notification.alert({message: ''+data.mensaje, title:"Intellibanks"});
   });
      };
     
-    $scope.cat={};
+  $scope.cat={};
    $http.get('http://empowerlabs.com/intellibanks/data/EmpowerLabsIntra/areasecomm.php').
    success(function(data){
     $scope.cat=data;
@@ -323,39 +371,75 @@ ons.notification.alert({message: ''+data.mensaje, title:"Intellibanks"});
 
 
 
+
+module.controller('videoController', function($scope, $dataBloc) {
+  $scope.item=$dataBloc.selectedItem;
+  $scope.urlVideo = $dataBloc.selectedItem.Url;
+  videos = document.querySelectorAll("video");
+  video = videos[0];
+  video.src = ''+$dataBloc.selectedItem.Url;
+});
+
+module.controller('AudioController', function($scope, $dataBloc) {
+  $scope.item=$dataBloc.selectedItem;
+  $scope.urlAudio = $dataBloc.selectedItem.Url;
+  audios = document.querySelectorAll("Audio");
+  audio = audios[0];
+  audio.src = ''+$dataBloc.selectedItem.Url;
+});
+
+module.controller('ImagenController', function($scope, $dataBloc) {
+    $scope.item=$dataBloc.selectedItem;
+  $scope.urlImagen = $dataBloc.selectedItem.Url;
+  Imagenes = document.querySelectorAll("img");
+  Imagen = Imagenes[0]; 
+  Imagen.src = ''+$dataBloc.selectedItem.Url;
+});
+
 module.controller('BlocController', function($scope, $dataBloc, $http) {
-    $scope.items = todosBloc;  
-    $http.get('http://empowerlabs.com/proyectos/trackersAPI/mblocs2/todos.php').
-  success(function(data, status, headers, config) {
-  	data.reverse();
-    $dataBloc.items=data;
-    todosBloc=data;
-    $scope.items = $dataBloc.items;  
-    $scope.showPap = function(item) {
+  $scope.items = todosBloc;
+  $http.get('http://empowerlabs.com/proyectos/trackersAPI/mblocs2/todos.php').success(function(data, status, headers, config) {
+    data.reverse();
+    $dataBloc.items = data;
+    todosBloc = data;
+    $scope.items = $dataBloc.items;
+    $scope.showBloc = function(item) {
       var selectedItem = item;
       $dataBloc.selectedItem = selectedItem;
-      $scope.ons.navigator.pushPage('detailMBloc.html', {title : selectedItem.title});
+      if (item.Format == 'Video') {
+        //alert(''+item.Format);
+        $scope.ons.navigator.pushPage('video.html');
+      }
+      else if(item.Format == 'Audio')
+      {
+        $scope.ons.navigator.pushPage('Audio.html');
+      }
+      else if(item.Format == 'Imagen')
+      {
+        $scope.ons.navigator.pushPage('Imagen.html');
+      }
+      else
+      {}
+
     };
-  }).
-  error(function(data, status, headers, config) {
-  	
-  });
-  });
+  }).error(function(data, status, headers, config) {
 
-  module.factory('$dataBloc', function() {
-      var data = {};
-      
-      data.items = todosBloc;
-      
-      return data;
   });
+});
 
+module.factory('$dataBloc', function() {
+  var data = {};
 
-  module.controller('DetailBlocController', function($scope, $dataBloc) {
-    $scope.item = $dataBloc.selectedItem;
-    //navigator.notification.vibrate(2000); //milliseconds
-	//navigator.notification.beep(2); // numbr of times
-  });
+  data.items = todosBloc;
+
+  return data;
+});
+
+module.controller('DetailBlocController', function($scope, $dataBloc) {
+  $scope.item = $dataBloc.selectedItem;
+  //navigator.notification.vibrate(2000); //milliseconds
+  //navigator.notification.beep(2); // numbr of times
+});
 
   module.controller('PAPController', function($scope, $dataPAP, $http) {
     $scope.items = todosPAP;  
@@ -390,52 +474,65 @@ module.controller('BlocController', function($scope, $dataBloc, $http) {
 	//navigator.notification.beep(2); // numbr of times
   });
 
+  
   module.controller('WorkflowController', function($scope) {
-  	$scope.cuentas=function(){
-  		$scope.ons.navigator.pushPage('modCuentas.html');
-  	};
+    $scope.cuentas=function(){
+      $scope.ons.navigator.pushPage('modCuentas.html');
+    };
     $scope.contactos=function(){
-  		$scope.ons.navigator.pushPage('modContactos.html');
-  	};
+      $scope.ons.navigator.pushPage('modContactos.html');
+    };
     $scope.pipeline=function(){
-  		$scope.ons.navigator.pushPage('modPipeline.html');
-  	};
+      $scope.ons.navigator.pushPage('modPipeline.html');
+    };
+    
+
   });
   
   module.controller('modCuentasController', function($scope,$http) {
-  	$scope.base={};
-  	$http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/JSonCuentas.php').
-  	success(function(data){
-  		$scope.base=data;
-  	});
-  	$scope.cuentaOne=function(b){
-  		selectedCuenta=b;
-  		$scope.ons.navigator.pushPage('cuentas.html');
-  	};
+    $scope.base={};
+    $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/JSonCuentas.php').
+    success(function(data){
+      $scope.base=data;
+    });
+    $scope.cuentaOne=function(b){
+      selectedCuenta=b;
+      $scope.ons.navigator.pushPage('cuentas.html');
+    };
+
+    $scope.AltaCuenta=function(){
+      $scope.ons.navigator.pushPage('modAltaCuenta.html');
+    };
   });
 
   module.controller('modContactosController', function($scope,$http) {
-  	$scope.base={};
-  	$http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/JSonContactos.php').
-  	success(function(data){
-  		$scope.base=data;
-  	});
-  	$scope.contactoOne=function(b){
-  		selectedContacto=b;
-  		$scope.ons.navigator.pushPage('contactos.html');
-  	};
+    $scope.base={};
+    $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/JSonContactos.php').
+    success(function(data){
+      $scope.base=data;
+    });
+    $scope.contactoOne=function(b){
+      selectedContacto=b;
+      $scope.ons.navigator.pushPage('contactos.html');
+    };
+    $scope.AltaContacto=function(){
+      $scope.ons.navigator.pushPage('modAltaContacto.html');
+    };
   });
 
   module.controller('modPipelineController', function($scope,$http) {
-  	$scope.base={};
-  	$http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/JSonPipeline.php').
-  	success(function(data){
-  		$scope.base=data;
-  	});
-  	$scope.pipelineOne=function(b){
-  		selectedPipeline=b;
-  		$scope.ons.navigator.pushPage('pipeline.html');
-  	};
+    $scope.base={};
+    $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/JSonPipeline.php').
+    success(function(data){
+      $scope.base=data;
+    });
+    $scope.pipelineOne=function(b){
+      selectedPipeline=b;
+      $scope.ons.navigator.pushPage('pipeline.html');
+    };
+    $scope.AltaSales=function(){
+      $scope.ons.navigator.pushPage('modAltaSales.html');
+    };
   });
 
 
@@ -451,4 +548,83 @@ module.controller('BlocController', function($scope, $dataBloc, $http) {
 
    module.controller('pipelineController', function($scope) {
     $scope.b = selectedPipeline;
+  });
+
+
+  module.controller('Nuevo2Controller',function($scope,$http){
+     $scope.formData = {};
+     $scope.formData.autor='Main.'+user;
+     $scope.push2=function(){
+        modal.show();
+        $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/AltaCuenta.php?name='+$scope.formData.name+'&autor='+$scope.formData.autor+'&accountmanager='+$scope.formData.accountmanager+'&address='+$scope.formData.address+'&city='+$scope.formData.city+'&state='+$scope.formData.state+'&contry='+$scope.formData.contry+'&phone='+$scope.formData.phone+'&fax='+$scope.formData.fax+'&url='+$scope.formData.url).
+  success(function(data, status, headers, config) {
+     modal.hide();
+     $scope.alta.$setPristine();
+     $scope.formData = {};
+ons.notification.alert({message: ''+data.mensaje, title:"Intellibanks"});
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+     };
+  });
+
+  module.controller('NuevoContactoController',function($scope,$http){
+     $scope.formData = {};
+     $scope.formData.autor='Main.'+user;
+     $scope.pushCto=function(){
+        modal.show();
+        $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/AltaContacto.php?greeting='+$scope.formData.greeting+'&name='+$scope.formData.name+'&title='+$scope.formData.title+'&phone='+$scope.formData.phone+'&mobile='+$scope.formData.mobile+'&fax='+$scope.formData.fax+'&email='+$scope.formData.email+'&skype='+$scope.formData.skype+'&url='+$scope.formData.url+'&manager='+$scope.formData.manager+'&account='+$scope.formData.account+'&type='+$scope.formData.type+'&status='+$scope.formData.status+'&autor='+$scope.formData.autor).
+  success(function(data, status, headers, config) {
+     modal.hide();
+     $scope.alta.$setPristine();
+     $scope.formData = {};
+ons.notification.alert({message: ''+data.mensaje, title:"Intellibanks"});
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+     };
+
+   $scope.bass={};
+    $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/SelectCuentas.php').
+    success(function(data){
+      $scope.bass=data;
+    });
+
+  });
+
+
+  module.controller('NuevoSalesController',function($scope,$http){
+     $scope.formData = {};
+     $scope.formData.autor='Main.'+user;
+     $scope.pushSales=function(){
+        modal.show();
+        $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/AltaSales.php?stage='+$scope.formData.stage+'&autor='+$scope.formData.autor+'&description='+$scope.formData.description+'&account='+$scope.formData.account+'&contact='+$scope.formData.contact+'&assignedto='+$scope.formData.assignedto+'&opportunity='+$scope.formData.opportunity+'&producttype='+$scope.formData.producttype+'&nextcontactdate='+$scope.formData.nextcontactdate+'&nextcontacttype='+$scope.formData.nextcontacttype+'&nextcontactstatus='+$scope.formData.nextcontactstatus+'&nextcontactdetails='+$scope.formData.nextcontactdetails+'&createdate='+$scope.formData.createdate+'&targetclosedate='+$scope.formData.targetclosedate+'&actualclosedate='+$scope.formData.actualclosedate).
+  success(function(data, status, headers, config) {
+     modal.hide();
+     $scope.alta.$setPristine();
+     $scope.formData = {};
+ons.notification.alert({message: ''+data.mensaje, title:"Intellibanks"});
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+     };
+
+$scope.bass={};
+    $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/SelectCuentas.php').
+    success(function(data){
+      $scope.bass=data;
+    });
+
+$scope.boss={};
+    $http.get('http://www.empowerlabs.com/intellibanks/data/Sandbox/SelectContactos.php').
+    success(function(data){
+      $scope.boss=data;
+    });
+
   });
